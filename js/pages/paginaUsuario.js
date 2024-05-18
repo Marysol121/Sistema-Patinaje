419675b97cbd466373555cda4687d9d1ee755013
oscarian <andres.merino96@ucuenca.edu.ec>
2024-05-18 18:07:49 -0500

    123

--- js/pages/paginaUsuario.js
+++ js/pages/paginaUsuario.js
@@ -2,7 +2,7 @@ import { crearEventos } from '../components/eventos/componentesEventos.js';
 import { crearFootterUsuario } from '../components/footer/footerUsuario.js';
 import { crearNavUsuario } from '../components/nav/navUsuario.js';
 import { crearNoticias } from '../components/noticias/componentesNoticias.js';
-import { crearTienda } from '../components/inicio/componentesTienda.js';
+import { crearTienda } from '../components/tienda/componentesTienda.js';
 
 
 export function cargarPagina(){
